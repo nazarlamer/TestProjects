e83c53abed73b5bf82f9c40efc60a6548dc43c1e
Nazarlamer <Dubbing@ukr.net>
2019-05-11 20:55:52 +0300

    no message

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <QDataStream>
+#include <QFile>
 #include "mydata.h"
 
 int main()
@@ -12,6 +14,43 @@ int main()
     if (ofs.is_open()) {
         ofs << myData;
         std::cout << "Console out: " << "FILE REC" << std::endl;
-
     }
+
+    QFile file( "d:\\Askmem_3\\NV\\data\\vv.dat" );
+        if(file.open( QFile::ReadOnly ) )
+        {
+
+            QByteArray iContents = file.readAll();
+            std::cout << "Console out: " << iContents[0] << std::endl;
+            unsigned short i = (unsigned char) iContents.at(432);
+            std::cout << "Console out: " << i << std::endl;
+            file.seek(0);
+            QByteArray block = file.read(4);
+
+
+
+            if(file.seek(4) )
+            {
+                char * dataRead = new char[ sizeof( qint32 ) ];
+                qint64 numBytesRead = file.read(dataRead, sizeof( qint32 ) );
+
+                if( numBytesRead == sizeof( qint32 ) )
+                {
+                    QByteArray dataBytes(dataRead);
+
+                    bool ok = false;
+                    qint32 value = dataBytes.toInt( &ok ); // "value" ends up 0
+
+                    if(ok)
+                    {
+                        int cout = value; // never gets here
+                        std::cout << "Console out: " << cout << std::endl;
+                    }else {
+                        std::cout << "Console out: " << "NOT JR" << std::endl;
+                    }
+                }
+            }
+        }else {
+            std::cout << "Console out: " << "NO FILE" << std::endl;
+        }
 }
